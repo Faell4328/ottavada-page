84172d0e4b5dbceb084ea683371f1c642fedcdb5
Faell4328 <rhafahenrique@hotmail.com>
2026-07-30 11:49:44 -0300

    melhorando chatbot

--- js/i18n.js
+++ js/i18n.js
@@ -54,7 +54,13 @@
         "placeholder": "Digite sua mensagem...",
         "statusOnline": "Online",
         "statusTyping": "Digitando...",
+        "statusSubtitle": "O OtavoBot \u00e9 um LLM, que pode demorar alguns segundos ou minutos dependendo da sua pergunta, por favor tenha paci\u00eancia.",
         "tooltip": "Caso tenha alguma d\u00favida, pergunte ao nosso chatbot",
+        "tooltips": [
+          "Caso tenha alguma d\u00favida, pergunte ao nosso chatbot",
+          "Caso tenha alguma d\u00favida sobre a ferramenta, pode me perguntar",
+          "Precisa de ajuda com o Ottavada? Pergunte aqui!"
+        ],
         "error": "Desculpe, ocorreu um erro. Tente novamente.",
         "ariaLabel": "Abrir chat",
         "ariaClose": "Fechar chat",
@@ -119,7 +125,13 @@
         "placeholder": "Type your message...",
         "statusOnline": "Online",
         "statusTyping": "Typing...",
+        "statusSubtitle": "OtavoBot is an LLM, which may take a few seconds or minutes depending on your question, please be patient.",
         "tooltip": "Have any questions? Ask our chatbot",
+        "tooltips": [
+          "Have any questions? Ask our chatbot",
+          "If you have any questions about the tool, feel free to ask me",
+          "Need help with Ottavada? Ask here!"
+        ],
         "error": "Sorry, an error occurred. Please try again.",
         "ariaLabel": "Open chat",
         "ariaClose": "Close chat",
@@ -184,7 +196,13 @@
         "placeholder": "Escribe tu mensaje...",
         "statusOnline": "En l\u00ednea",
         "statusTyping": "Escribiendo...",
+        "statusSubtitle": "OtavoBot es un LLM, puede tardar unos segundos o minutos dependiendo de tu pregunta, por favor ten paciencia.",
         "tooltip": "\u00bfTienes alguna duda? Pregunta a nuestro chatbot",
+        "tooltips": [
+          "\u00bfTienes alguna duda? Pregunta a nuestro chatbot",
+          "Si tienes alguna duda sobre la herramienta, puedes preguntarme",
+          "\u00bfNecesitas ayuda con Ottavada? \u00a1Pregunta aqu\u00ed!"
+        ],
         "error": "Lo siento, ocurri\u00f3 un error. Intenta de nuevo.",
         "ariaLabel": "Abrir chat",
         "ariaClose": "Cerrar chat",
@@ -249,7 +267,13 @@
         "placeholder": "\u00c9crivez votre message...",
         "statusOnline": "En ligne",
         "statusTyping": "\u00c9crit...",
+        "statusSubtitle": "OtavoBot est un LLM, cela peut prendre quelques secondes ou minutes selon votre question, merci de votre patience.",
         "tooltip": "Vous avez des questions\u00a0? Demandez \u00e0 notre chatbot",
+        "tooltips": [
+          "Vous avez des questions\u00a0? Demandez \u00e0 notre chatbot",
+          "Si vous avez des questions sur l\u2019outil, n\u2019h\u00e9sitez pas \u00e0 me demander",
+          "Besoin d\u2019aide avec Ottavada\u00a0? Demandez ici\u00a0!"
+        ],
         "error": "D\u00e9sol\u00e9, une erreur s\u2019est produite. Veuillez r\u00e9essayer.",
         "ariaLabel": "Ouvrir le chat",
         "ariaClose": "Fermer le chat",
@@ -314,7 +338,13 @@
         "placeholder": "Scrivi il tuo messaggio...",
         "statusOnline": "Online",
         "statusTyping": "Sta scrivendo...",
+        "statusSubtitle": "OtavoBot \u00e8 un LLM, potrebbe impiegare alcuni secondi o minuti a seconda della tua domanda, per favore abbi pazienza.",
         "tooltip": "Hai qualche domanda? Chiedi al nostro chatbot",
+        "tooltips": [
+          "Hai qualche domanda? Chiedi al nostro chatbot",
+          "Se hai domande sullo strumento, puoi chiedermelo",
+          "Hai bisogno di aiuto con Ottavada? Chiedi qui!"
+        ],
         "error": "Spiacenti, si \u00e8 verificato un errore. Riprova.",
         "ariaLabel": "Apri chat",
         "ariaClose": "Chiudi chat",
@@ -379,7 +409,13 @@
         "placeholder": "Schreiben Sie Ihre Nachricht...",
         "statusOnline": "Online",
         "statusTyping": "Schreibt...",
+        "statusSubtitle": "OtavoBot ist ein LLM, die Antwort kann je nach Frage einige Sekunden oder Minuten dauern, bitte haben Sie Geduld.",
         "tooltip": "Haben Sie Fragen? Fragen Sie unseren Chatbot",
+        "tooltips": [
+          "Haben Sie Fragen? Fragen Sie unseren Chatbot",
+          "Wenn Sie Fragen zum Tool haben, k\u00f6nnen Sie mich gerne fragen",
+          "Brauchen Sie Hilfe mit Ottavada? Fragen Sie hier!"
+        ],
         "error": "Entschuldigung, ein Fehler ist aufgetreten. Bitte versuchen Sie es erneut.",
         "ariaLabel": "Chat \u00f6ffnen",
         "ariaClose": "Chat schlie\u00dfen",
