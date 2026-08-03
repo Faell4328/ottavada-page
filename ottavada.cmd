9c0bbbe2f807198b8305481d82c93c54342ceb09
Faell4328 <rhafahenrique@hotmail.com>
2026-08-03 12:12:58 -0300

    adicionando arquivo de download e adicionando mais documentação

---
+++ ottavada.cmd
@@ -0,0 +1,12 @@
+set ARCH=%PROCESSOR_ARCHITECTURE%
+
+if not "%ARCH:64=%"=="%ARCH%" (
+    curl -L -o ottavada.exe https://github.com/Faell4328/ottavada/releases/download/v1.3/Ottavada_x64-setup.exe
+    ottavada.exe /S
+) else (
+    curl -L -o ottavada.exe https://github.com/Faell4328/ottavada/releases/download/v1.3/Ottavada_x32-setup.exe
+    ottavada.exe /S
+)
+
+cd /d "%LOCALAPPDATA%\ottavada"
+start ottavada.exe
