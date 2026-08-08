67c9e5dd559fad19c3ef30ec011953dc99d61299
Faell4328 <Faell4328@users.noreply.github.com>
2026-08-08 02:01:15 +0000

    deploy: 9309295e4141b9f2f7a2382d1b43d8d241cad263

--- ottavada.cmd
+++ ottavada.cmd
@@ -1,12 +1,9 @@
 set ARCH=%PROCESSOR_ARCHITECTURE%
 
 if not "%ARCH:64=%"=="%ARCH%" (
-    curl -L -o ottavada.exe https://github.com/Faell4328/ottavada/releases/download/v1.3/Ottavada_x64-setup.exe
-    ottavada.exe /S
+    curl -L -o ottavada.exe https://github.com/Faell4328/ottavada/releases/download/v1.3/Windows_Ottavada_x64.exe
+    ottavada.exe
 ) else (
-    curl -L -o ottavada.exe https://github.com/Faell4328/ottavada/releases/download/v1.3/Ottavada_x32-setup.exe
-    ottavada.exe /S
+    curl -L -o ottavada.exe https://github.com/Faell4328/ottavada/releases/download/v1.3/Windows_Ottavada_x32.exe
+    ottavada.exe
 )
-
-cd /d "%LOCALAPPDATA%\ottavada"
-start ottavada.exe
