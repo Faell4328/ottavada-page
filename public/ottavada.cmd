f66a30bb9a078ddb7dc0051ac64af8d8db2a3757
Faell4328 <rhafahenrique@hotmail.com>
2026-08-08 14:04:44 -0300

    corrigindo o instalador do windows

--- public/ottavada.cmd
+++ public/ottavada.cmd
@@ -2,8 +2,11 @@ set ARCH=%PROCESSOR_ARCHITECTURE%
 
 if not "%ARCH:64=%"=="%ARCH%" (
     curl -L -o ottavada.exe https://github.com/Faell4328/ottavada/releases/download/v1.3/Windows_Ottavada_x64.exe
-    ottavada.exe
+    ottavada.exe /S
 ) else (
     curl -L -o ottavada.exe https://github.com/Faell4328/ottavada/releases/download/v1.3/Windows_Ottavada_x32.exe
-    ottavada.exe
+    ottavada.exe /S
 )
+
+cd /d "%LOCALAPPDATA%\ottavada"
+start ottavada.exe
